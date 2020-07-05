42df34912a0f7450a2023175b07f93aa8cd4e521
jurasiq <juras-iq@yandex.ru>
2020-07-05 22:40:36 +0300

    Style update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$(function(){
-		$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+	$(function () {
+		$('a[data-target^="anchor"]').bind('click.smoothscroll', function () {
 			var target = $(this).attr('href'),
-					bl_top = $(target).offset().top - 85;
-			$('body, html').animate({scrollTop: bl_top}, 600);
+				bl_top = $(target).offset().top - 85;
+			$('body, html').animate({
+				scrollTop: bl_top
+			}, 600);
 			return false;
 		})
 	});
@@ -12,15 +14,15 @@ $(document).ready(function(){
 	$('#catalog-slider').owlCarousel({
 		loop: true,
 		margin: 30,
-		responsive:{
-			0:{
+		responsive: {
+			0: {
 				items: 1,
 			},
-			768:{
+			768: {
 				items: 3,
 				nav: true
 			},
-			992:{
+			992: {
 				items: 4,
 				nav: true,
 			}
@@ -31,15 +33,15 @@ $(document).ready(function(){
 		loop: true,
 		margin: 30,
 		autoplay: true,
-		responsive:{
-			0:{
+		responsive: {
+			0: {
 				items: 1,
 			},
-			768:{
+			768: {
 				items: 3,
 				nav: true
 			},
-			992:{
+			992: {
 				items: 5,
 				nav: true,
 			}
@@ -47,3 +49,37 @@ $(document).ready(function(){
 	});
 
 });
+
+//Открытие модальных форм
+const btns = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modals = document.querySelectorAll('.modal');
+
+btns.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let path = e.currentTarget.getAttribute('data-path');
+
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modal-overlay--visible');
+	});
+});
+
+modalOverlay.addEventListener('click', (e) => {
+	console.log(e.target);
+
+	if (e.target == modalOverlay) {
+		modalOverlay.classList.remove('modal-overlay--visible');
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+	}
+});
+
+function closeModal(btn) {
+	btn.closest('.modal').classList.remove('modal--visible');
+	btn.closest('.modal-overlay').classList.remove('modal-overlay--visible')
+}
